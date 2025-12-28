4063852b4bbcb399e62f3bf50fa2291f72876ede
jiang-zhexin <162887873+jiang-zhexin@users.noreply.github.com>
2025-12-28 11:25:06 +0800

    feat: add chrome as a new certificate store option

--- certificate.ts
+++ certificate.ts
@@ -19,7 +19,7 @@ export interface certificate {
     /**
      * @default system
      */
-    store?: 'system' | 'mozilla' | 'none'
+    store?: 'system' | 'mozilla' | 'chrome' | 'none'
     certificate?: listable<string>
     certificate_path?: listable<string>
     certificate_directory_path?: listable<string>
